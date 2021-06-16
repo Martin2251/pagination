380bbbdfec72c98d146336dc171d045b66e740f0
martin lowe <martinlowe673@gmail.com>
2021-06-16 06:45:35 +0100

    added search bar although .map is having issues

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 function Header() {
-  return <h3 className="title">Martin Movie</h3>;
+  return <h3 className="title">Martin Movie App</h3>;
 }
 
 export default Header;
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,28 +1,67 @@
 import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
-function MainContent() {
+function MainPage() {
   const [listMovie, setListMovie] = useState([]);
-  const [nrPages, setNrPages] = useState(0);
+  // the movies the set allows you to store it
+  const [aBPages, setAbPages] = useState(0);
+  // pages
   const [currentPage, setCurrentPage] = useState(1);
+  //pages going through start at 1 because of a page
+  const [searchMovie, setMovie] = useState("shrek");
+
   useEffect(() => {
-    fetch(`http://www.omdbapi.com/?s=shrek&page=${currentPage}&apikey=24885019`)
+    fetch(
+      `http://www.omdbapi.com/?s=${searchMovie}&page=${currentPage}&apikey=24885019`
+    )
       .then((response) => response.json())
       .then((data) => {
         setListMovie(data.Search);
-        // use ceil to round up to the largest integer
+        // use ceil to round up to the largest integer and return back 10 results
         const table = Math.ceil(data.totalResults / 10);
-        setNrPages(table);
+        setAbPages(table);
         console.log(data);
       });
   }, [currentPage]);
+
   const pagesList = [];
-  for (let i = 1; i <= nrPages; i++) {
+  for (let i = 1; i <= aBPages; i++) {
     pagesList.push(i);
     // adds new items to the array
+    // I need a for loop to go through it
+    // returns back information from the API
+    // abs returns the absolute value of a number
+    //film is what i want to pass into the function
   }
+  // now needs a function to search the movies
+  function getMovies(event) {
+    fetch(
+      `http://www.omdbapi.com/?s=${searchMovie}&page=${currentPage}&apikey=24885019`
+    )
+      .then((response) => response.json())
+
+      .then((data) => {
+        setListMovie(data.search);
+        const table = Math.ceil(data.totalResults / 10);
+        setAbPages(table);
+        console.log(data);
+        const pageList = [];
+        for (let i = 1; i <= aBPages; i++) {
+          pageList.push(i);
+        }
+      });
+  }
+
   return (
     <div>
-      <h1 className="subtitle">Shrek</h1>
+      <div>
+        <SearchBar
+          onChange={(event) => setMovie(event.target.value)}
+          type="text "
+          placeholder="Search movie.."
+        />
+        <SearchButton onClick={getMovies}>Search</SearchButton>
+      </div>
       <div className="box">
         <div className="row" style={{ backgroundColor: "#3E363F" }}>
           <h4 className="col one">Title</h4>
@@ -30,7 +69,7 @@ function MainContent() {
           <h4 className="col three">Type</h4>
         </div>
 
-        {listMovie.map(function (film) {
+        {listMovie?.map(function (film) {
           return (
             <div className="row" style={{ backgroundColor: "#FF8700" }}>
               <p className="col one">{film.Title}</p>
@@ -51,6 +90,7 @@ function MainContent() {
         <button
           style={{ backgroundColor: currentPage ? "#55868C" : "#04395E" }}
           onClick={(event) => setCurrentPage(Math.abs(currentPage - 1))}
+          // maths.abs returns the value of any given number
         >
           Prev
         </button>
@@ -69,9 +109,14 @@ function MainContent() {
             );
           })}
         </div>
-        <button 
+        <button
           style={{ backgroundColor: currentPage ? "#55868C" : "#04395E" }}
-          onClick={(event) => setCurrentPage(Math.abs(currentPage + 1))}> Next
+          onClick={(event) => setCurrentPage(Math.abs(currentPage + 1))}
+          // math.abs is a function that returns the value of the number
+          // you want the event to trigger when the user clicks
+        >
+          {" "}
+          Next
         </button>
         <button
           style={{ backgroundColor: currentPage ? "#55868C" : "#04395E" }}
@@ -84,4 +129,13 @@ function MainContent() {
   );
 }
 
-export default MainContent;
+export default MainPage;
+
+const SearchBar = styled.input`
+  font-size: 1.5rem;
+`;
+const SearchButton = styled.button`
+  font-size: 1.5rem;
+  background-color: #ba1b1d;
+  cursor: pointer;
+`;
